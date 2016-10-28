fdee36b241318aa38cdf7b693c2ec3b5f915901b
David Cohen <david.cohen@west.cmu.edu>
2016-10-27 22:28:36 -0700

    added required=False to boolean form field

--- core/forms.py
+++ core/forms.py
@@ -2,6 +2,6 @@ from django import forms
 from s3direct.widgets import S3DirectWidget
 
 class UploadGeoTagset(forms.Form):
-    small_test = forms.BooleanField()
+    small_test = forms.BooleanField(required=False)
     format_id = forms.CharField(max_length=30)
     data_file = forms.URLField(widget=S3DirectWidget(dest="csv_upload"))
