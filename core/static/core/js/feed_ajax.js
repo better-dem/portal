426f33ded462d6c9200bb2d43281e7b6cc514b14
David Cohen <davidogbodfog@gmail.com>
2017-01-12 16:01:39 -0800

    fixed variable scope bug in feed_ajax.js, cleaned up in a couple other places

--- core/static/core/js/feed_ajax.js
+++ core/static/core/js/feed_ajax.js
@@ -46,10 +46,8 @@ function setContent(elem, val){
 
 function revealElement(elem){
     var display_style = elem.getAttribute("data-display-style");
-    console.log("tag has a data-display-style"+display_style)
     if (display_style == null){
 	if (elem.tagName == "SPAN"){
-	    console.log("elem is a span!")
 	    elem.style.display = "inline";					
 	} else {
 	    elem.style.display = "block";
@@ -59,32 +57,32 @@ function revealElement(elem){
     }
 }
 
-function get_form_response_cb(result_element_id){
+var get_form_response_cb = function(result_element_id){
     return function(response_content, status){
 	// use the content to populate child nodes and set display of child nodes of the result element
-	console.log("ajax form response. status:"+status)
-	console.log("ajax form response. response_content:"+response_content)
-	console.log(JSON.stringify(response_content))
+	console.log("ajax form response. status:"+status);
+	console.log("ajax form response. response_content:"+response_content);
+	console.log(JSON.stringify(response_content));
 	// response content is a json string
 	var e = document.getElementById(result_element_id);
 	for (var key in response_content){
 	    if (response_content.hasOwnProperty(key)){
 		var val = response_content[key];
 		if (key == "reveal" || key == "hide"){
-		    for ( i = 0; i < val.length; i++ ){
+		    for (var  i = 0; i < val.length; i++ ){
 			var child = e.querySelector("."+val[i]);
 			if (child != null){
 			    if (key == "reveal"){
 				revealElement(child);
 			    } else if (key == "hide"){
-				child.style.display = "none"
+				child.style.display = "none";
 			    }
 			}
 		    }
 		} else {
 		    var child = e.querySelector("."+key)
 		    if (child != null){
-			setContent(child, val)
+			setContent(child, val);
 			if (child.style.display = "none"){
 			    revealElement(child);
 			}
@@ -92,26 +90,35 @@ function get_form_response_cb(result_element_id){
 		}
 	    }
 	}
-    }
+    };
 }
 
 // submit a form
-function submit_ajax_form(url, content, cb) {
-    $.post(url, content, cb)
+var submit_ajax_form = function(url, content, cb) {
+    $.post(url, content, cb);
 }
 
-// register event triggers on all forms on the page based on their attributes
-var portal_ajax_form_submit_buttons = document.getElementsByClassName("portal_ajax_submit");
-console.log("portal ajax form submit buttons:")
-console.log(portal_ajax_form_submit_buttons)
-
-for ( i = 0; i < portal_ajax_form_submit_buttons.length; i++){
-    console.log("registering button:" + i)
-    var b = portal_ajax_form_submit_buttons[i]
+// register event triggers for a button based on its attributes
+var register_event_trigger = function(b){
     b.addEventListener("click", function(e) {
-	submit_ajax_form(b.getAttribute("data-target-url"), 
+	submit_ajax_form(b.getAttribute("data-target-url"),
 			 document.getElementById(b.getAttribute("data-target-element")).value,
 			 get_form_response_cb(b.getAttribute("data-result-element")))
     }, false);
-};
+}
+
+// register ajax form event triggers on all forms on the page
+var ajax_form_setup = function(){
+    var portal_ajax_form_submit_buttons = document.getElementsByClassName("portal_ajax_submit");
+    console.log("portal ajax form submit buttons:");
+    console.log(portal_ajax_form_submit_buttons);
+
+    for ( var i = 0; i < portal_ajax_form_submit_buttons.length; i++){
+	console.log("registering button:" + i);
+	var b = portal_ajax_form_submit_buttons[i];
+	register_event_trigger(b)
+    };
+}
+
+ajax_form_setup()
 
