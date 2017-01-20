cb144ec782313ebfa8bc0a8fd8ded7f237d3d6c1
David Cohen <davidogbodfog@gmail.com>
2017-01-19 21:20:41 -0800

    added fancy splash page with parallax

--- core/urls.py
+++ core/urls.py
@@ -11,11 +11,12 @@ urlpatterns = [
     url(r'^report_issues/apps/(?P<app_name>.+)/(?P<action_name>.+)/(?P<object_id>.+)$', views.report_issues, name="report_issues"),
     url(r'^accounts/', include('registration.backends.hmac.urls')),
     url(r'^accounts/profile', views.show_profile, name="profile"),
-    url(r'^$', views.feed, name="feed"),
-    url(r'^upload_dataset$', views.upload_dataset, name="upload_dataset"),
-    url(r'^testgeo$', views.test_geo, name="test_geo"),
-    url(r'^tags$', views.tags, name="tags"),
-    url(r'^update_tags$', views.update_profile_tags, name="update_tags"),
+    url(r'^$', views.splash_if_anon, name="home"),
+    url(r'^feed/$', views.feed, name="feed"),
+    url(r'^upload_dataset/$', views.upload_dataset, name="upload_dataset"),
+    url(r'^testgeo/$', views.test_geo, name="test_geo"),
+    url(r'^tags/$', views.tags, name="tags"),
+    url(r'^update_tags/$', views.update_profile_tags, name="update_tags"),
     url(r'^apps/(?P<app_name>.+)/(?P<action_name>.+)/(?P<object_id>.+)$', views.app_view_relay, name="app_view_relay"),
     url(forms.tag_aac.get_url_pattern(), forms.tag_aac.ajax_autocomplete_view, name="")
 
