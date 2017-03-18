c32367dfa2cfbe9539404602235b723163a3dbbb
David Cohen <davidogbodfog@gmail.com>
2017-03-18 16:33:36 -0700

    added improved schema checking to budget json, updated participate page to reflect it

--- city_budgeting/models.py
+++ city_budgeting/models.py
@@ -6,21 +6,83 @@ import random
 import json
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
+import traceback
+from collections import Counter
 
 def validate_budget_json(json_string):
+    def assert_schema(d, schema):
+        """ Ensure d follows schema exactly. All keys are required. Schema is just for depth=1 """
+        assert(len(d.keys()) == len(schema))
+        for k in schema.keys():
+            assert(k in d.keys())
+            try:
+                assert(isinstance(d[k], schema[k]))
+            except AssertionError:
+                raise ValidationError(_("%(key)s isn't a %(type)s"), params={'key': unicode(d[k]), 'type': unicode(schema[k])})
+
+    def assert_close_enough(n1, n2):
+        """ Ensure that the difference between two numbers is within acceptable limit, rounding / float arithmetic errors """
+        try: 
+            assert(abs(1.0*n1 - 1.0*n2) < 1E-8*(n1+n2))
+        except AssertionError:
+            raise ValidationError(_("%(n1)s is supposed to be equal to %(n2)s"), params={'n1': unicode(n1), 'n2': unicode(n2)})
+
+
     obj = None
     try:
         obj = json.loads(json_string)
     except Exception as e:
-        raise ValidationError(_("Error parsing json:%(error)s"), params={'error': str(e)})
+        raise ValidationError(_("Error parsing json:%(error)s"), params={'error': unicode(e)})
     try:
-        assert(len(obj.keys()) == 3)
-        assert("revenues" in obj.keys())
-        assert("funds" in obj.keys()) 
-        assert("expenses" in obj.keys())
+        assert_schema(obj, {"revenues": dict, "funds": dict, "expenses": dict})
+
+        r = obj["revenues"]
+        assert_schema(r, {"checksum": float, "category_checksums": dict, "items": list})
+        checksum = r["checksum"]
+        categories = r["category_checksums"].keys()
+        assert_schema(r["category_checksums"], {k: float for k in categories})
+        assert_close_enough(sum(r["category_checksums"].values()), checksum)
+        category_sums = Counter({c:0 for c in categories})
+        for i in r["items"]:
+            assert_schema(i, {"id": int, "name": unicode, "category": unicode,"amount": float, "description": unicode})
+            assert(i["category"] in categories)
+            category_sums.update({i["category"]: i["amount"]})
+        for c in categories:
+            assert_close_enough(category_sums[c], r["category_checksums"][c])
+
+        f = obj["funds"] 
+        assert_schema(f, {"checksum": float, "category_checksums": dict, "items": list})
+        checksum = f["checksum"]
+        categories = f["category_checksums"].keys()
+        assert_schema(f["category_checksums"], {k: float for k in categories})
+        assert_close_enough(sum(f["category_checksums"].values()), checksum)
+        category_sums = Counter({c:0 for c in categories})
+        for i in f["items"]:
+            assert_schema(i, {"id": int, "name": unicode, "category": unicode,"starting_balance": float})
+            assert(i["category"] in categories)
+            category_sums.update({i["category"]: i["starting_balance"]})
+        for c in categories:
+            assert_close_enough(category_sums[c], f["category_checksums"][c])
+
+        e = obj["expenses"]
+        assert_schema(e, {"checksum": float, "category_checksums": dict, "items": list})
+        checksum = e["checksum"]
+        categories = e["category_checksums"].keys()
+        assert_schema(e["category_checksums"], {k: float for k in categories})
+        assert_close_enough(sum(e["category_checksums"].values()), checksum)
+        category_sums = Counter({c:0 for c in categories})
+        for i in e["items"]:
+            assert_schema(i, {"id": int, "name": unicode, "category": unicode,"amount": float, "description": unicode})
+            assert(i["category"] in categories)
+            category_sums.update({i["category"]: i["amount"]})
+        for c in categories:
+            assert_close_enough(category_sums[c], e["category_checksums"][c])
+            
         # much more here
     except Exception as e:
-        raise ValidationError(("The budget JSON string must contain exactly 3 keys at the top level: revenues, funds, and expenses",))
+        sys.stderr.write(traceback.format_exc())
+        sys.stderr.flush()
+        raise ValidationError(_("Json string doesn't follow budget json schema: %(error)s"), params={'error': str(e)})
 
 class CityBudgetingProject(cm.ParticipationProject):
     city = models.ForeignKey(cm.GeoTag, on_delete = models.CASCADE)
@@ -41,7 +103,7 @@ class CityBudgetingProject(cm.ParticipationProject):
         return set()
 
     def set_name(self):
-        self.name = "City Budget Outreach Project for "+project.city.get_name()
+        self.name = "City Budget Outreach Project for "+self.city.get_name()
 
 class CityBudgetingItem(cm.ParticipationItem):
     """
