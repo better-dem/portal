1bbf221737601880763d65d24af9d0a877b6f5d1
David Cohen <davidogbodfog@gmail.com>
2017-04-13 15:52:32 -0700

    pull and zip state files, don't add anything to DB yet

--- legislators/management/commands/legislators_update.py
+++ legislators/management/commands/legislators_update.py
@@ -4,40 +4,70 @@ from core import models as cm
 import argparse
 import datetime
 import sys
-
+import pyopenstates
+import zipfile
+import os
 
 class Command(BaseCommand):
     help = """
-    A utility to update all legislators
+    A utility to update all legislators using the openstates API
     usage: python manage.py legislators_update -m <mode>
     """
 
     def handle(self, *args, **options):
         mode = options["mode"]
+        workingdir = "openstates_data_workingdir"
+        jsonzip = "state-json.zip"
 
-        if mode == "fix":
-            for o in cm.ParticipationProject.objects.all():
-                try:
-                    i = o.get_inherited_instance()
-                except:
-                    sys.stdout.write("participation project found whose inherited instance can't be determined:{}".format(o.name)+"\n")
-                    sys.stdout.flush()
-                    sys.stdout.write("deleting project\n")
-                    o.delete()
-                    sys.stdout.write("object deleted\n")
-        elif mode == "display":
-            for o in cm.ParticipationProject.objects.all():
-                try:
-                    i = o.get_inherited_instance()
-                except:
-                    sys.stdout.write("participation project found whose inherited instance can't be determined:{}".format(o.name)+"\n")
-                    sys.stdout.flush()
+        metadata = pyopenstates.get_metadata()
+        tags = cm.GeoTag.objects.filter(feature_type="SP")
+        sys.stdout.write("number of state / province tags:"+unicode(len(tags))+"\n")
+        sys.stdout.write("state names:"+unicode([t.name for t in tags])+"\n")
+        sys.stdout.flush()
+        for state in metadata:
+            sys.stdout.write("deleting existing state data...\n")
+            sys.stdout.flush()
+            os.system("rm -rf "+workingdir+"/*")
+            os.system("rm "+jsonzip)
+            sys.stdout.write(state["name"]+"\n")
+            sys.stdout.flush()
+            tag = None
+            try:
+                tag = cm.GeoTag.objects.get(feature_type="SP", name=state["name"])
+            except:
+                sys.stdout.write("state not in geotags\n")
+                continue
 
-        else:
-            raise Exception("unknown mode:"+str(mode))
+            try:
+                with open(jsonzip, "wb") as state_zip_file:
+                    sys.stdout.write("downloading...\n")
+                    pyopenstates.download_bulk_data(state["abbreviation"], state_zip_file)
+
+                sys.stdout.write("unzipping...\n")
+                zip_ref = zipfile.ZipFile(jsonzip, 'r')
+                zip_ref.extractall(workingdir)
+                zip_ref.close()
+
+            except zipfile.BadZipfile:
+                sys.stdout.write("there was a problem reading this state's zipfile\n")
+                sys.stdout.flush()
+                continue
+
+            sys.stdout.write("results:\n")
+            sys.stdout.write("contents:"+str(os.listdir(workingdir))+"\n")
+            legislator_files = os.listdir(workingdir+"/legislators")
+            sys.stdout.write("number of legislators:"+str(len(legislator_files))+"\n")
+            bill_files = os.listdir(workingdir+"/bills")
+            sys.stdout.write("number of bills:"+str(len(bill_files))+"\n")
+            committee_files = os.listdir(workingdir+"/committees")
+            sys.stdout.write("number of committees:"+str(len(committee_files))+"\n")
+            sys.stdout.flush()
+
         sys.stdout.write("DONE\n")
         sys.stdout.flush()
+        os.system("rm -rf "+workingdir+"/*")
+        os.system("rm "+jsonzip)
          
     def add_arguments(self, parser):
         parser.add_argument('-m', '--mode', required=True, type=str, help="mode", action='store')
-        parser.add_argument('-t', '--troubletype', required=True, type=str, help="trouble type", action='store')
+
--- tool_review/models.py
+++ tool_review/models.py
@@ -3,17 +3,19 @@ from __future__ import unicode_literals
 from django.db import models
 from core import models as cm
 
-CHAMBERS = (("UP", "upper"), ("LO", "lower"))
+TOOL_CATEGORIES = (("OD", "Open Data"), ("ED", "Educational Material"), ("PA", "Policy Analysis"), ("CL", "Collaborative Problem-Solving"), ("UN", "Unclassified"))
+PROJECT_CATEGORIES = (("GV","Government Project"), ("OS", "Open source project"), ("CO", "Commercial Product"), ("UN", "Unclassified"), ("NC", "Nonprofit Closed Source"))
 
 class ToolReviewProject(cm.ParticipationProject):
-    open_states_leg_id = models.CharField(max_length=100)
-    open_states_active = models.BooleanField()
-    open_states_state = models.CharField(max_length=50)
-    photo_url = models.URLField()
-    webpage_url = models.URLField()
-    chamber = models.CharField(max_length=2, choices=CHAMBERS, blank=True, null=True)
-    district = models.CharField(max_length=50)
-    email = models.CharField(max_length=100)
+    # name <- tool's name
+    url = models.URLField()
+    screenshot_filename = models.FilePathField(max_length=500, blank=True)
+    summary = models.TextField()
+    youtube_video_id = models.CharField(max_length=100, blank=True)
+    review_blog_post = models.URLField(blank=True)
+    tags = models.ManyToManyField(cm.Tag)
+    tool_category = models.CharField(max_length=2, choices=TOOL_CATEGORIES, default="UN")
+    project_category = models.CharField(max_length=2, choices=PROJECT_CATEGORIES, default="UN")
     
     def update_items(self):
         if ToolReviewItem.objects.filter(participation_project=self, is_active=True).count()==0:
@@ -32,6 +34,7 @@ class ToolReviewItem(cm.ParticipationItem):
         self.tags.add(*self.participation_project.tags.all())
 
     def set_display_image(self):
-        self.display_image_file = "tool_review/img/default.png"
-
-
+        if not self.participation_project.screenshot_filename is None:
+            self.display_image_file = self.participation_project.screenshot_filename
+        # else:
+        #     self.display_image_file = "tool_review/img/default.png"
