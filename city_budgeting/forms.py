757d112a65783ff5f3b93fab0d384fc18aa39a34
David Cohen <davidogbodfog@gmail.com>
2017-05-30 11:36:00 -0700

    upload excel file for budget app

--- city_budgeting/forms.py
+++ city_budgeting/forms.py
@@ -1,15 +1,63 @@
 from django import forms
 from django.forms.widgets import Textarea, DateInput, NumberInput
+from s3direct.widgets import S3DirectWidget
 from core import forms as cf
 import json
 import sys
 from widgets import forms as wf
 from city_budgeting.models import validate_budget_json
 
+EXAMPLE_JSON="""{"description": "This is an example budget JSON for the fictional city of Happyville.",
+ "funds":
+ {
+     "description":"Happyville has two funds: the general fund and the youth sports fund.",
+     "items": 
+     [
+ {"id":0, "name":"General Fund", "category":"General Government Operating Funds", "description":"The General Fund is the primary operating fund of the City. The General Fund is used to account for resources traditionally associated with government which are not required by law or by sound financial management practice to be accounted for in another fund."},
+ {"id":1, "name":"Youth Sports Fund", "category":"Special Revenue Funds", "description": "This fund is to support youth sports programs in Happyville."}
+     ]
+ }, 
+ "revenues": 
+ {
+     "description":"All of Happyville's revenue is collected by Tom the Taxman.",
+     "items": 
+     [
+ {"id":0, "name":"Property Tax", "category": "Taxes", "amount":1500.0, "description":"", "target_fund":0}, 
+ {"id":1, "name":"Sales Tax", "category": "Taxes", "amount":300.0, "description":"Happyville's sales tax rate is 3%", "target_fund":0}, 
+ {"id":2, "name":"Selling Hotdogs", "category": "Fees for Services", "amount":200.0, "description":"", "target_fund":1}, 
+ {"id":3, "name":"Business Licenses", "category": "Permits and Licenses", "amount": 8540.0, "description":"", "target_fund":0},
+ {"id":4, "name":"Home Renovation Permits", "category": "Permits and Licenses", "amount": 230.0, "description":"", "target_fund":0}
+     ]
+ }, 
+ "expenses":
+ {
+     "description":"Happyville is a well-oiled fiscal machine.",
+     "items": 
+     [
+ {"id":0, "name":"Salaries and Wages", "category":"Salaries and Wages", "amount":100.0, "description":"", "origin_fund": 0},
+ {"id":1, "name":"Benefits", "category":"Benefits", "amount":100.0, "description":"", "origin_fund": 0},
+ {"id":2, "name":"Supplies", "category":"Supplies", "amount":300.0, "description":"", "origin_fund": 0},
+ {"id":3, "name":"Other Services and Charges", "category":"Other Services and Charges", "amount":250.0, "description":"", "origin_fund": 0},
+ {"id":4, "name":"Reserves", "category":"Reserves", "amount":800.0, "description":"", "origin_fund": 0},
+ {"id":5, "name":"Supplies", "category":"Supplies", "amount":100.0, "description":"Purchased gloves for the baseball team", "origin_fund": 1},
+ {"id":6, "name":"Salaries and Wages", "category":"Salaries and Wages", "amount":100.0, "description":"Paid Old Jim to mow the lawn", "origin_fund": 1}
+     ]
+ }
+}"""
+
 class CreateProjectForm(forms.Form):
+    name = forms.CharField(label="Project Name")
     fiscal_period_start = forms.DateField(widget = wf.DatePickerJQueryWidget)
     fiscal_period_end = forms.DateField(widget = wf.DatePickerJQueryWidget)
-    budget_json = forms.CharField(widget=Textarea, validators=[validate_budget_json])
+    
+    budget_description = forms.CharField()
+    revenues_description = forms.CharField()
+    funds_description = forms.CharField()
+    expenses_description = forms.CharField()
+
+    budget_excel_file = forms.URLField(widget=S3DirectWidget(dest="file_upload"))
+
+    # budget_json = forms.CharField(widget=Textarea(attrs={'style':"width:100%;", 'rows': 20, 'cols':2}), validators=[validate_budget_json], initial=EXAMPLE_JSON)
     budget_url = forms.URLField()
     city = cf.tag_aac.get_new_form_field(required=False)
 
--- city_budgeting/views.py
+++ city_budgeting/views.py
@@ -9,6 +9,7 @@ import core.forms as cf
 import core.models as cm
 import core.tasks as ct
 import json
+from urlparse import urlsplit
 
 def new_project(request, group=None):
     (profile, permissions, is_default) = cv.get_profile_and_permissions(request)
@@ -18,11 +19,16 @@ def new_project(request, group=None):
         if form.is_valid():
             project = CityBudgetingProject()
             project.city = cf.get_best_final_matching_tag(form.cleaned_data["city"]).geotag
-            project.fiscal_period_start = form.cleaned_data["fiscal_period_start"]
-            project.fiscal_period_end = form.cleaned_data["fiscal_period_end"]
-            project.budget_json = form.cleaned_data["budget_json"]
-            project.set_name()
-            project.budget_url = form.cleaned_data["budget_url"]
+
+            simple_fields = ["fiscal_period_start", "fiscal_period_end", "budget_url", "name", "budget_description", "revenues_description", "funds_description", "expenses_description"]
+            for k in simple_fields:
+                project.__dict__[k] = form.cleaned_data[k]
+
+            excel_file_url = form.cleaned_data["budget_excel_file"]
+            path_with_bucket_and_leading_slash = urlsplit(excel_file_url)[2]
+            path_without_bucket = "/".join(path_with_bucket_and_leading_slash.split("/")[2:])
+            project.budget_excel_file = path_without_bucket
+
             project.owner_profile = profile
             project.group=group
             project.save()
@@ -39,7 +45,7 @@ def new_project(request, group=None):
 def edit_project(request, project_id):
     (profile, permissions, is_default) = cv.get_profile_and_permissions(request)
     project = get_object_or_404(CityBudgetingProject, pk=project_id) 
-    simple_fields = ["fiscal_period_start", "fiscal_period_end", "budget_json", "budget_url"]
+    simple_fields = ["fiscal_period_start", "fiscal_period_end", "budget_url", "name"]
     if request.method == 'POST':
         form = CreateProjectForm(request.POST)
         changes = set()
@@ -53,13 +59,11 @@ def edit_project(request, project_id):
             if not project.city == form_city:
                 project.city = form_city
                 changes.add("city")
-                project.set_name()
-                changes.add("name")
             project.save()
 
             if "name" in changes or "city" in changes:
                 ct.finalize_project(project)
-            return render(request, 'core/thanks.html', {"action_description": "editing your city budget project", "link": "/apps/city_budgeting/administer_project/"+str(project.id)})
+            return render(request, 'core/thanks.html', {"action_description": "editing your budget transparency project", "link": "/apps/city_budgeting/administer_project/"+str(project.id)})
         else:
             return render(request, 'core/generic_form.html', {'form': form, 'action_path' : request.path})
 
@@ -82,8 +86,8 @@ def participate(request, item_id):
     context = cv.get_default_og_metadata(request, item)
     project = item.participation_project.citybudgetingproject
     data = json.loads(project.budget_json)
-    fund_map = {i["id"]: i for i in data["funds"]["items"]}
-    revenue_categories = {i[0]: i[1] for i in enumerate(set([x["category"] for x in data["revenues"]["items"]]))}
-    expense_categories = {i[0]: i[1] for i in enumerate(set([x["category"] for x in data["expenses"]["items"]]))}
+    fund_map = {i["id"]: i for i in data["funds"]}
+    revenue_categories = {i[0]: i[1] for i in enumerate(set([x["category"] for x in data["revenues"]]))}
+    expense_categories = {i[0]: i[1] for i in enumerate(set([x["category"] for x in data["expenses"]]))}
     context.update({"project": project, "data":data, "site": os.environ["SITE"], "item":item, "fund_map":fund_map, "revenue_categories": revenue_categories, "expense_categories": expense_categories})
     return render(request, "city_budgeting/participate.html", context)
